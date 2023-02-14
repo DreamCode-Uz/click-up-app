9a0714cfc9ce36af47b0a1cadc410b1f36871162
Dilshod Fayzullayev <javadeveloperbackend999@gmail.com>
2023-02-14 20:56:21 +0500

    :tada: initial commit click-up project

---
+++ src/main/java/uz/pdp/clickupsecondpart/ClickUpSecondPartApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.clickupsecondpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickUpSecondPartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickUpSecondPartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/clickupsecondpart/ClickUpSecondPartApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.clickupsecondpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickUpSecondPartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
